7842b9750efc57f28ac265626ced5436fedbc5b5
ahyoon99 <kimay9623@naver.com>
2024-07-29 20:24:16 +0900

    schema.sql, data.sql 세팅(setting)하기
    Related to: #37

---
+++ src/main/resources/h2/schema.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS RESTAURANT;
+
+-- 식당
+CREATE TABLE RESTAURANT
+(
+    index INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,         -- 가게명, 음식명
+    category VARCHAR(255) NOT NULL,      -- 카테고리
+    address VARCHAR(255) NOT NULL,       -- 주소
+    roadAddress VARCHAR(255),   -- 도로명 주소
+    homePageLink VARCHAR(255),  -- 홈페이지 주소
+    imageLink VARCHAR(255),     -- 음식, 가게 이미지 주소
+    isVisit VARCHAR(1) NOT NULL,       -- 방문 여부
+    visitCount INTEGER NOT NULL,         -- 방문 횟수
+    lastVisitDate TIMESTAMP    -- 마지막 방문 날짜
+);
